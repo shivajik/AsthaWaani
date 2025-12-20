e219b5383aee2713b34d649ea47e301ec5da7371
mayankkamriya <kamriyamayank45@gmail.com>
2025-12-20 11:04:00 +0530

    add space in blog page top area

--- client/src/pages/blog.tsx
+++ client/src/pages/blog.tsx
@@ -33,7 +33,7 @@ export default function Blog() {
 
   return (
     <div className="min-h-screen bg-white dark:bg-black">
-      <div className="container mx-auto px-4 py-12 md:py-16">
+      <div className="container mx-auto px-4 pt-24 md:pt-32 pb-12 md:pb-16">
         <h1 className="text-4xl md:text-5xl font-bold mb-12 text-center">
           {title}
         </h1>
