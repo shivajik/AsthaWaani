da2272ec9b4b8486cea59dc6ab5e86b58454555f
mayankkamriya <kamriyamayank45@gmail.com>
2026-04-20 12:35:06 +0530

    stop the popof appearing when first thme open website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ import DynamicPage from "@/pages/dynamic-page";
 import CommunityPage from "./pages/community";
 import JoinPartnersPage from "./pages/join-partners";
 import ApplyVakta from "./pages/apply-vakta";
-import { PromoPopup } from "./components/promo-popup";
+// import { PromoPopup } from "./components/promo-popup";
 
 function Router() {
   return (
@@ -63,7 +63,7 @@ function AppContent() {
         <Router />
       </main>
       {!isAdmin && <Footer />}
-      {!isAdmin && <PromoPopup />}
+      {/* {!isAdmin && <PromoPopup />} */}
       <Toaster />
     </div>
   );
